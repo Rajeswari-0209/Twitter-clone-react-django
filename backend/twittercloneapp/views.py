1736073dc20343b4284a4fe01fb8cecc097a3481
Rajeswari.V <rajeshwari@techis.io>
2021-06-08 10:35:54 +0530

    ordered by descending

--- backend/twittercloneapp/views.py
+++ backend/twittercloneapp/views.py
@@ -4,6 +4,8 @@ from twittercloneapp.serializers import TweetSerializer
 from .models import Tweet
 
 class TweetViewSet(viewsets.ModelViewSet):
-    queryset = Tweet.objects.all().order_by()
+    # queryset = Tweet.objects.all().order_by()
+    queryset = Tweet.objects.order_by('created_at').reverse().all()[:20]
     serializer_class = TweetSerializer
    
+Tweet.objects.order_by('created_at').reverse().all()[:20]
